e05e78845ef2a9b306e52c9776c0fb6d74a62ceb
randyRivera0 <rariberm@espol.edu.ec>
2024-07-27 13:01:34 -0500

    Initial commit

---
+++ src/main/java/ec/edu/espol/adivinar/Adivinar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.adivinar;
+
+/**
+ *
+ * @author Gecko
+ */
+public class Adivinar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
